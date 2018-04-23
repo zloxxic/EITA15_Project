b29e3c15bc92a6207652068cd29a9646ebff241a
kvacksalvaren <38491100+kvacksalvaren@users.noreply.github.com>
2018-04-23 15:36:14 +0200

    Add files via upload

---
+++ ShipTest.c
@@ -0,0 +1,59 @@
+/*
+ * ShipTest.c
+ *
+ *  Created on: 23 apr. 2018
+ *      Author: os2422el-s
+ */
+#include "ship.h"
+
+int main(void) {
+	int net[10][10];
+	int i, j;
+	for (i = 0; i < 10; i++) {
+		for (j = 0; j < 10; j++) {
+			net[i][j] = 0;
+		}
+	}
+	Ship* ship = Ship_create(5, 5, 3, 2);
+	int nY = ship->y;
+	int nX = ship->x;
+	//net[ship->Ship_x][ship->y] = 1;
+	i = 0;
+	j = 0;
+	switch (ship->dir) {
+	case 0:
+
+		for (i = 0; i < (ship->seg); i++) {
+			net[ship->x][nY] = 1;
+			nY--;
+		}
+		break;
+	case 1:
+
+		for (i = 0; i < (ship->seg); i++) {
+			net[nX][ship->y] = 1;
+			nX++;
+		}
+		break;
+	case 2:
+		for (i = 0; i < (ship->seg); i++) {
+			net[ship->x][nY] = 1;
+			nY++;
+		}
+		break;
+	case 3:
+		for (i = 0; i < (ship->seg); i++) {
+			net[nX][ship->y] = 1;
+			nX--;
+		}
+		break;
+	}
+	for(i = 0; i < 10; i++){
+
+		for(j = 0; j < 10; j++){
+			printf(net[i][j] + " ");
+		}
+		printf("\n");
+	}
+}
+
