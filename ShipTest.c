df5ca35a746095d32ea1564f6ec98e54328a4faf
vipaulsen <victor_paulsen@hotmail.com>
2018-05-17 16:35:53 +0200

    hejsvejs

--- ShipTest.c
+++ ShipTest.c
@@ -9,73 +9,100 @@
 #include <stdlib.h>
 
 int main(void) {
-	int net[10][10];
+	int net[12][12];
 	int i, j;
-	for (i = 0; i < 10; i++) {
-		for (j = 0; j < 10; j++) {
+	for (i = 0; i < 12; i++) {
+		for (j = 0; j < 12; j++) {
 			net[i][j] = 0;
 		}
 	}
 	Ship* ship = Ship_create(5, 5, 3, 2);
-	Ship* ship1 = Ship_create(0, 0, 4, 2);
-	int nY = ship->y;
-	int nX = ship->x;
-	int nY1 = ship1->y;
-	int nX1 = ship1->x;
-	int ch1, ch2;
-	ch1 = getch();
+	Ship* ship1 = Ship_create(5, 5, 4, 0);
+	Ship* ship3 = Ship_create(2, 2, 4, 1);
+	Ship* ship2 = Ship_create(7, 5, 2, 1);
+
+	Ship_place(ship,net);
+	Ship_place(ship1,net);
+	Ship_place(ship3,net);
+	Ship_place(ship2,net);
+
+//	int nY = ship->y;
+//	int nX = ship->x;
+//	int nY1 = ship1->y;
+//	int nX1 = ship1->x;
 	//net[ship->Ship_x][ship->y] = 1;
 	i = 0;
 	j = 0;
 	//kollar att x och y-värde ligger innanför matrisen, kanske inte behövs
-	if (nX < 10 && nX >= 0 && nY < 10 && nY >= 0) {
-		switch (ship1->dir) {
-		case 0:
-			//kollar att skeppet inte sträcker sig utanför matrisen
-			if (nY - ship->seg >= 0 && net[nX][nY]==0) {
-				for (i = 0; i < (ship->seg); i++) {
+//	if (nX < 10 && nX >= 0 && nY < 10 && nY >= 0) {
+//		switch (ship->dir) {
+//		case 0:
+//
+//			for (int k = 0; k < (ship->seg); k++) {
+//				if (net[nY - k][nX] != 0) {
+//					break;
+//				}
+//			}
+//			//kollar att skeppet inte sträcker sig utanför matrisen
+//			if (nY - ship->seg >= 0) {
+//				for (i = 0; i < (ship->seg); i++) {
+//
+//					net[nY][nX] = 1;
+//					nY--;
+//					net[nX1][nY1] = 1;
+//					nY1--;
+//
+//				}
+//			}
+//			break;
+//		case 1:
+//			for (int k = 0; k < (ship->seg); k++) {
+//				if (net[nY][nX + k] != 0) {
+//					break;
+//				}
+//			}
+//			if (nX + ship->seg < 10) {
+//				for (i = 0; i < (ship->seg); i++) {
+//					net[nY][nX] = 1;
+//					nX++;
+//				}
+//			}
+//			break;
+//		case 2:
+//			for (int k = 0; k < (ship->seg); k++) {
+//				if (net[(nY + k)][nX] != 0) {
+//					break;
+//				}
+//			}
+//			if (nY + ship->seg < 10) {
+//				for (i = 0; i < (ship->seg); i++) {
+//					net[nY][nX] = 1;
+//					nY++;
+//					net[nY1][nX1] = 1;
+//					nY1++;
+//				}
+//			}
+//			break;
+//		case 3:
+//			for (int k = 0; k < (ship->seg); k++) {
+//				if (net[(nY)][nX - k] != 0) {
+//					break;
+//				}
+//			}
+//			if (nX - ship->seg >= 0) {
+//				for (i = 0; i < (ship->seg); i++) {
+//					net[nY][nX] = 1;
+//					nX--;
+//					net[nY1][nX1] = 1;
+//					nX1--;
+//				}
+//			}
+//			break;
+//		}
+//	}
+	for (i = 0; i < 12; i++) {
 
-					net[nY][nX] = 1;
-					nY--;
-					net[nX1][nY1] = 1;
-					nY1--;
-
-				}
-			}
-			break;
-		case 1:
-			if (nX + ship->seg < 10) {
-				for (i = 0; i < (ship->seg); i++) {
-					net[nY][nX] = 1;
-					nX++;
-				}
-			}
-			break;
-		case 2:
-			if (nY + ship->seg < 10) {
-				for (i = 0; i < (ship->seg); i++) {
-					net[nY][nX] = 1;
-					nY++;
-					net[nY1][nX1] = 1;
-					nY1++;
-				}
-			}
-			break;
-		case 3:
-			if (nX - ship->seg >= 0) {
-				for (i = 0; i < (ship->seg); i++) {
-					net[nY][nX] = 1;
-					nX--;
-					net[nY1][nX1] = 1;
-					nX1--;
-				}
-			}
-			break;
-		}
-	}
-	for (i = 0; i < 10; i++) {
-
-		for (j = 0; j < 10; j++) {
+		for (j = 0; j < 12; j++) {
 			if (net[i][j] == 0) {
 				printf("a");
 			} else {
